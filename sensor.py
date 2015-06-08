f712cb3eb78c52a10c1c60fb7329833ca0f59c6f
eddienigma <jaseymour@gmail.com>
2015-06-07 23:39:16 -0500

    Working version of sensor.py that reads BMP180 and AM2302

--- sensor.py
+++ sensor.py
@@ -3,12 +3,19 @@
 # jaseymour@gmail.com
 
 import Adafruit_BMP.BMP085 as BMP180
-sensor = BMP180.BMP085(mode=BMP180.BMP085_ULTRAHIGHRES)
+import Adafruit_DHT
 
-cTemp = sensor.read_temperature()
+sensor1 = BMP180.BMP085(mode=BMP180.BMP085_ULTRAHIGHRES)
+sensor2 = Adafruit_DHT.AM2302
+pin = 23
+
+cTemp = sensor1.read_temperature()
 fTemp = ((cTemp * 9) / 5) + 32
+hPa = sensor1.read_pressure() / 100
 
-print 'Temp = {0:0.2f} *C'.format(cTemp)
-print 'Temp = {0:0.2f} *F'.format(fTemp)
-print 'Pressure = {0:0.2f} Pa'.format(sensor.read_pressure())
+rh, dht_temperature = Adafruit_DHT.read_retry(sensor2, pin)
 
+print 'Temperature = {0:0.2f} *C'.format(cTemp)
+print 'Temperature = {0:0.2f} *F'.format(fTemp)
+print 'Pressure = {0:0.2f} hPa'.format(hPa)
+print 'Humidity={0:0.1f}%'.format(rh)
